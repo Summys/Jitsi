5f92f26888cf873bbe8b372daf6e2048a95d1970
Marian Iordache <mi@taktik.com>
2021-11-19 22:59:18 +0200

    jitsi on react native 66

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { AppNavigator } from './navigators';
+import { Text } from 'react-native';
 
 const App = () => {
   return (
     <Provider store={store}>
+      {/* <Text>Hello</Text> */}
       <NavigationContainer>
         <AppNavigator />
       </NavigationContainer >
